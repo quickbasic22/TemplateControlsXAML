71e68dc659cbd1977eb54b7a26f3c192f64e23b5
David Morrow <quickbasic44@outlook.com>
2022-06-15 20:18:14 -0400

    Add project files.

---
+++ TemplateControlsXAML/TemplateControlsXAML/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TemplateControlsXAML
+{
+    public partial class MainPage : ContentPage
+    {
+        // undone
+
+        // hack
+        // UnresolvedMergeConflict
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TemplateControlsXAML/TemplateControlsXAML/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateControlsXAML.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Location { get; set; }
+    }
+}
